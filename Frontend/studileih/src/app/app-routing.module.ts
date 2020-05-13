04141fc0ae17435391cab373b3785a3fb0416c07
Georg Kromer <georgkromer@protonmail.com>
2020-05-13 07:28:07 +0200

    sidebar and fake posts/users loading

--- Frontend/studileih/src/app/app-routing.module.ts
+++ Frontend/studileih/src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DetailsComponent } from './details/details.component';
+import { UsersComponent } from './users/users.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { PostsComponent } from './posts/posts.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: UsersComponent
+  },
+  {
+    path: 'details/:id',
+    component: DetailsComponent
+  },
+  {
+    path: 'posts',
+    component: PostsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ Frontend/studileih/src/app/sidebar/sidebar.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+
+export class SidebarComponent implements OnInit {
+
+  constructor() { 
+  }
+
+  ngOnInit() {}
+
+}
